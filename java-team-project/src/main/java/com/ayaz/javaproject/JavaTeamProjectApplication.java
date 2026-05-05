654b3993f0697322dddf9d605975b57e9df7c422
Dadun <ayazkinmail@gmail.com>
2026-05-05 22:02:58 +0300

    Spring boot project created

---
+++ java-team-project/src/main/java/com/ayaz/javaproject/JavaTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ayaz.javaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTeamProjectApplication.class, args);
+    }
+
+}
---
+++ java-team-project/src/test/java/com/ayaz/javaproject/JavaTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayaz.javaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
