4d1bd179fa6b21c9d3390e06e72345e58958abc7
kosta <kostacimaev@gmail.com>
2026-05-12 18:31:37 +0300

    EQ-1 add equipment entity

---
+++ java-team-project/src/main/java/com/booking/javaproject/equipment/model/Equipment.java
@@ -0,0 +1,36 @@
+package com.booking.javaproject.equipment.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "equipment")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Equipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 120)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean active = true;
+
+    public Equipment(String name) {
+        this.name = name;
+    }
+}
